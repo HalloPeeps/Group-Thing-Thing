f862ccd934b2ed1bd493c0eef34087598f64de01
Hannrep4 <149887125+Hannrep4@users.noreply.github.com>
2025-05-05 22:15:55 -0400

    Update views.py
    Added view functions that will display HTML templates.

--- product_showcase/products/views.py
+++ product_showcase/products/views.py
@@ -2,5 +2,27 @@ from django.shortcuts import render
 
 # Create your views here.
 
+# viewing product (still needs to be made): make a list of different apples, take specific apple from list depending on what product the user clicks on, pass that apple through a view function
+
 def landing_page(request):
     return render(request, "products/landing.html")
+
+def product_home(request):
+    return render(request, "products/product_home.html")
+    print("FOR TESTING PURPOSES: You're in the products page! Apples should be displayed here.")
+
+def product_detail(request):
+    return render(request, "products/view_product.html")
+    print("FOR TESTING PURPOSES: You should see this page after clicking on an apple!")
+
+def about_us(request):
+    print("FOR TESTING PURPOSES: Displays information about our development team!")
+    return render(request, "products/about_us.html")
+
+def our_project(request):
+    return render(request, "products/our_project.html")
+    print("FOR TESTING PURPOSES: Displays our project timeline!")
+
+def dummy_page(request):
+    print("FOR TESTING PURPOSES: Displays dummy page!")
+    return render(request, "products/dummy_page.html")
