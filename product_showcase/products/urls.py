aaa0b3b4c61d0d6a5ac932959c20efe38d588e50
Hannrep4 <149887125+Hannrep4@users.noreply.github.com>
2025-05-05 22:18:32 -0400

    Update urls.py
    Added additional URLs for views.

--- product_showcase/products/urls.py
+++ product_showcase/products/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from products import views
-
 urlpatterns = [
-        path("", views.landing_page, name="landing"), # "default" page (displays when there's no / in the URL)
-        # about us page
-        # our project page
+        path("", views.landing_page, name="landing-page"), # "default" page (shows when there's no / in the URL)
+        path("products", views.product_home, name="product-home"),
+        path("products/apple", views.product_detail, name="product-detail"), # still needs to be changed so that it's dynamic (i.e.: /products/honeycrisp-apple)
+        path("about-us", views.about_us, name="about-us"),
+        path("our-project", views.our_project, name="our-project"),
+        path("dummy-page", views.dummy_page, name="dummy-page")
 ]
