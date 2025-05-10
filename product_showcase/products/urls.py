af5ba90e49a481a1360352d306b3b10a27fc9a25
Hannrep4 <149887125+Hannrep4@users.noreply.github.com>
2025-05-10 12:21:31 -0400

    Update urls.py
    Created a dynamic URL for viewing products. Also created URLs for cart & feedback form. The "dummy page" is not a necessary part of our final site as it was used for testing purposes.

--- product_showcase/products/urls.py
+++ product_showcase/products/urls.py
@@ -3,8 +3,10 @@ from products import views
 urlpatterns = [
         path("", views.landing_page, name="landing-page"), # "default" page (shows when there's no / in the URL)
         path("products", views.product_home, name="product-home"),
-        path("products/apple", views.product_detail, name="product-detail"), # still needs to be changed so that it's dynamic (i.e.: /products/honeycrisp-apple)
+        path("products/<str:apple>/", views.product_detail, name="product-detail"),
         path("about-us", views.about_us, name="about-us"),
         path("our-project", views.our_project, name="our-project"),
-        path("dummy-page", views.dummy_page, name="dummy-page")
+        path("view-cart", views.view_cart, name="view-cart"),
+        path("feedback-form", views.log_message, name="feedback-form"),
+        path("dummy-page", views.dummy_page, name="dummy-page") # ignore; was just used for testing
 ]
